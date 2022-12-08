87efdf2c7f43a3ed6b7d9efb4fce0e8568c0747f
NisanEfe <madanoglu.nisanefe@gmail.com>
2022-12-08 12:12:00 +0300

    first commit

---
+++ array/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+
+	// var a [4]string
+
+	// a[0] = "ali"
+	// a[1] = "veli"
+	// a[2] = "ahmet"
+	// a[3] = "mehmet"
+	// fmt.Println(a)
+	// fmt.Println(a[0])
+	// fmt.Println(len(a))
+
+	// a := [4]string{"ali", "ayse", "veli", "deniz"}
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Println(a[i])
+	// }
+
+	// a := [4]int{12, 34, 16, 43}
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Println(a[i] + 3)
+	// }
+
+}

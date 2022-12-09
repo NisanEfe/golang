db36ec758285c7bc3a46a09cdd654cca2dfb7849
NisanEfe <madanoglu.nisanefe@gmail.com>
2022-12-09 13:39:47 +0300

    for

---
+++ for/main.go
@@ -0,0 +1,103 @@
+package main
+
+func main() {
+
+	// for i := 0; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// for i := 0; i < 100; i = i + 3 {
+	// 	fmt.Println(i)
+	// }
+
+	// for i := 20; i >= 0; i-- {
+	// 	fmt.Println(i)
+	// }
+
+	// i := 0
+	// for i = 0; i < 3; i++ {
+	// 	fmt.Println(i)
+	// }
+	// fmt.Println("Donguden sonra i:", i)
+
+	// sonsuz dongu
+	// for {
+	// 	fmt.Println("a")
+	// }
+	// for true {
+	// 	fmt.Println("a")
+	// }
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println("i dongusu --->", i)
+	// 	for j := 0; j < 5; j++ {
+	// 		fmt.Println(i, j)
+	// 	}
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// 	if i == 5 {
+	// 		break
+	// 	}
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	for k := 0; k < 5; k++ {
+	// 		if k == 1 {
+	// 			break
+	// 		}
+	// 		fmt.Println(i, k)
+	// 	}
+	// 	if i == 2 {
+	// 		break
+	// 	}
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	if i == 3 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// etiketli break
+	// ilk:
+	// 	for i := 0; i < 3; i++ {
+	// 		fmt.Println(i)
+	// 		break ilk
+	// 	}
+
+	// ilk:
+	// 	for i := 0; i < 3; i++ {
+	// 		for j := 0; j < 3; j++ {
+	// 			if j == 1 {
+	// 				break ilk
+	// 			}
+	// 			fmt.Println(i, j)
+	// 		}
+	// 	}
+
+	// ilk:
+	// 	for i := 0; i < 3; i++ {
+	// 	ikinci:
+	// 		for j := 0; j < 3; j++ {
+	// 			if j == 1 {
+	// 				break ilk
+	// 			} else {
+	// 				break ikinci
+	// 			}
+	// 			fmt.Println(i, j)
+	// 		}
+	// 	}
+
+	// etiketli continue
+	// ilk:
+	// 	for i := 0; i < 3; i++ {
+	// 		for j := 0; j < 3; j++ {
+	// 			fmt.Println(i, j)
+	// 			continue ilk
+	// 		}
+	// 	}
+
+}
